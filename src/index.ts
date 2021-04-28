6cd9027c37a1576f7d8fb054b4b0e8f16d83add9
jamashita <jamashita@arachnid.xyz>
2021-04-29 00:49:31 +0900

    feat: 🎸 remvoed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 export * from './plan';
 export * from './superposition';
 export * from './unscharferelation';
-
-export const inc = (i: number): number => {
-  return i++;
-};
-export const neg = (i: number): number => {
-  return -i;
-};
-export const twice = (i: number): number => {
-  return i * 2;
-};
