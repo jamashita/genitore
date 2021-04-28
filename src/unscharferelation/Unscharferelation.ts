a08262b2d705278989893e724b722cb55a79556f
jamashita <jamashita@arachnid.xyz>
2021-04-28 21:29:57 +0900

    feat: 🎸 import path

--- src/unscharferelation/Unscharferelation.ts
+++ src/unscharferelation/Unscharferelation.ts
@@ -1,7 +1,5 @@
 import { Consumer, Kind, Peek, Supplier, Suspicious, UnaryFunction } from '@jamashita/anden-type';
-import { Chrono } from '../Superposition/Chrono/Interface/Chrono';
-import { Detoxicated } from '../Superposition/Interface/Detoxicated';
-import { Superposition } from '../Superposition/Superposition';
+import { Superposition } from '../superposition/Superposition';
 import { Epoque } from './Epoque/Interface/Epoque';
 import { UnscharferelationError } from './Error/UnscharferelationError';
 import { Heisenberg } from './Heisenberg/Heisenberg';
